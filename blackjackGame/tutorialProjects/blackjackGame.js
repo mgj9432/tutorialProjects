771c6418195c256ed81ee00ae68bf77895e32ae4
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-12-14 14:07:18 +0000

    Add files via upload

---
+++ blackjackGame/tutorialProjects/blackjackGame.js
@@ -0,0 +1,29 @@
+let firstCard = 10;
+let secondCard = 11;
+
+let sum = firstCard + secondCard;
+
+let hasBlackJack = false;
+
+let isAlive = true;
+
+let message = "";
+
+// 2. Create a startGame() function. Move the conditional
+// below ( line 15-27) inside the body of the function.
+
+if (sum <= 20) {
+  message = "Do you want to draw a new card?";
+} else if (sum === 21) {
+  message = "Woohoo! You've got Blackjack";
+  hasBlackJack = true
+} else {
+  message = "You're out of the game!";
+  isAlive = false;
+}
+
+// 3. Log it out
+
+console.log(message);
+
+// test
---
+++ peopleCounter/peopleCounter.js
@@ -0,0 +1,19 @@
+let countEl = document.getElementById("count-el")
+let count = 0;
+
+let saveEl = document.getElementById("save-el")
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+// Document Object Model (DOM) aka how you use Javscript to modify a website
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  console.log(count);
+  countEl.textContent = 0;
+  count = 0;
+}
