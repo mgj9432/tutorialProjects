f5115c7a9c6cda419bc96e1e78388a50e65a4d13
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-12-15 10:21:32 +0000

    Created startGame() function. Moved previous conditional into startGame() function.

--- blackjackGame/blackjackGame.js
+++ blackjackGame/blackjackGame.js
@@ -11,17 +11,15 @@ let message = "";
 
 // 2. Create a startGame() function. Move the conditional
 // below ( line 15-27) inside the body of the function.
-
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Woohoo! You've got Blackjack";
-  hasBlackJack = true
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "Woohoo! You've got Blackjack";
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  console.log(message);
 }
-
-// 3. Log it out
-
-console.log(message);
