1d17180a337d94ffc98a93bf1cb2fca626ae8224
mgj9432 <101676643+mgj9432@users.noreply.github.com>
2022-12-19 15:15:57 +0000

    Add files via upload
    More progression with tutorial

--- blackjackGame/blackjackGame.js
+++ blackjackGame/blackjackGame.js
@@ -1,4 +1,4 @@
-let firstCard = 8;
+let firstCard = 10;
 let secondCard = 11;
 
 let sum = firstCard + secondCard;
@@ -9,23 +9,35 @@ let isAlive = true;
 
 let message = "";
 
-let messageEl = document.getElementById("message-el")
+let messageEl = document.getElementById("message-el");
 
-let sumEl = document.getElementById("sum-el")
+let sumEl = document.getElementById("sum-el");
+
+let cardsEl = document.getElementById("cards-el");
+
+// let sumEl = document.getElementById("sum-el") can be written
+// as follows:
+// let sumEl = document.querySelector("#sum-el")
 
 
 function startGame() {
-  sumEl.textContent = "Sum: " + sum
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
      message = "Woohoo! You've got Blackjack";
-     hasBlackJack = true
+     hasBlackJack = true;
   } else {
      message = "You're out of the game!";
      isAlive = false;
   }
   
-  messageEl.textContent = message
+  messageEl.textContent = message;
+}
+
+
+function newCard() {
+  alert("Drawing a new card from the deck!")
 }
 
